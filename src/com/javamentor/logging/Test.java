bf9b83a0fb51d77c58619129504f4ec583f47fd6
RomanLaypanov <rrrrraaammmmaaaazzz@gmail.com>
2025-05-04 01:03:30 +0300

    задача 4.4.8 первая проверка

--- src/com/javamentor/logging/Test.java
+++ src/com/javamentor/logging/Test.java
@@ -5,7 +5,7 @@ import java.util.logging.Logger;
 public class Test {
     static public final Logger LOGGER = Logger.getLogger(Test.class.getName());
 
-    static public void logging(){
+    static public void logging() {
 
         LOGGER.info("Все хорошо");
         LOGGER.warning("Произошла ошибка");
