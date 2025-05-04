61613f0cdc7eaaa4244b32306c142cfadb785230
RomanLaypanov <rrrrraaammmmaaaazzz@gmail.com>
2025-05-04 21:24:19 +0300

    задача 4.4.8 вторая проверка

--- src/com/javamentor/logging/Test.java
+++ src/com/javamentor/logging/Test.java
@@ -3,10 +3,10 @@ package com.javamentor.logging;
 import java.util.logging.Logger;
 
 public class Test {
-    static public final Logger LOGGER = Logger.getLogger(Test.class.getName());
 
     static public void logging() {
-        LOGGER.info("Все хорошо");
-        LOGGER.warning("Произошла ошибка");
+        final Logger LOG = Logger.getLogger(Test.class.getName());
+        LOG.info("Все хорошо");
+        LOG.warning("Произошла ошибка");
     }
 }
